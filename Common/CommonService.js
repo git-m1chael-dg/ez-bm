b33114e9d79d014076f9226713009734736c5686
XXXIII33 <msd.subscribe@gmail.com>
2015-07-22 08:20:15 +0800

    increase the version number

--- Common/CommonService.js
+++ Common/CommonService.js
@@ -3,7 +3,7 @@ hpiModule.service('CommonFunc', function ($http,toaster) {
             var self = this;
 
             self.version =  function(){
-              return "1.6";
+              return "1.8";
             };
 
             self.validateActivationCode = function (account) {
